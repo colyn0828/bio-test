0da5757ef18e912f838f47b890e23b81497038e9
Colyn Gao <colyngao@Colyns-MacBook-Pro.local>
2022-01-20 14:24:20 -0700

    finish the api

--- frontend-challenge/src/components/Card.js
+++ frontend-challenge/src/components/Card.js
@@ -2,32 +2,43 @@ import React, { useState } from "react";
 import "./Card.css";
 
 const Card = () => {
-  const [url, setUrl] = useState("");
+  const [inputUrl, setInputUrl] = useState(null);
   const [shortenUrl, setShortenUrl] = useState("");
+  const [validUrl, setValidUrl] = useState(true);
 
-  const onClickSubmit = () => {
-    fetch("https://api.shrtco.de/v2/")
-      .then((res) => res.json())
+  const onClickSubmit = (e) => {
+    if (!e.target.value.includes("https://")) {
+      setValidUrl(false);
+      setInputUrl("https://" + e.target.value);
+    }
+
+    fetch("https://api.shrtco.de/v2/shorten", {
+      method: "POST",
+      body: JSON.stringify({
+        original_link: inputUrl,
+      }),
+    })
+      .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setShortenUrl(data);
-      })
-      .catch((error) => console.log("Error:", error));
+        setShortenUrl(data.short_link);
+      });
+    setValidUrl(true);
   };
 
   return (
     <div>
       <div className="card">
-        <form onSubmit={onClickSubmit}>
+        <form className="form" onSubmit={onClickSubmit}>
           <input placeholder="Shorten a link here..." />
           <button className="btn">Shorten It!</button>
         </form>
       </div>
       <div className="feedback">
-        <p className="message"></p>
+        <p className="message">{validUrl ? "success" : "fail"}</p>
         <div className="url">
-          <div className="url-original"></div>
+          <div className="url-original">{inputUrl}</div>
           <div className="url-shorten">
+            {shortenUrl}
             <button className="btn">Copied!</button>
           </div>
         </div>
